9b8bf2a40a87ecb4e02f3a82f2b4d7292aaca4f5
caique-vianna <caiquevianna99@gmail.com>
2024-08-23 21:02:16 -0300

    modal add food

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,50 +1,52 @@
-import styled,  { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-display: flex;
-align-items: center;
+  display: flex;
+  align-items: center;
 
-background: #fff;
-border-radius: 8px;
-padding:18px 24px;
-width: 100%;
-font-size: 16px;
+  background: #fff;
+  border-radius: 8px;
+  padding: 18px 24px;
+  width: 100%;
+  font-size: 16px;
+  border: 1px solid #333;
 
-& + div {
+  & + div {
     margin-top: 24px;
-}
+  }
 
-h1 {
+  h1 {
     margin-bottom: 40px;
     font-weight: 600;
     font-size: 36px;
     line-height: 36px;
-}
+  }
 
-${props =>
-props.isFocused &&
-css`
-color: #ff9000:
-border-color: #ff9000:
-`}
-${props =>
-props.isFilled &&
-css`
-color: #ff9000:
-`}
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
 
-input {
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+  input {
     flex: 1;
     background: transparent;
     border: 0;
-    color: #b7b7cc;
+    color: #333;
 
     &::placeholder {
-        color: #b7b7cc;
+      color: #333;
     }
-}
+  }
 
-svg {
+  svg {
     margin-right: 16px;
-}
-`;
+  }
+`
--- src/components/ModalAddFood/index.jsx
+++ src/components/ModalAddFood/index.jsx
@@ -13,12 +13,22 @@ export const ModalAddFood = (props) => {
     <Modal isOpen={props.isOpen} setIsOpen={props.setIsOpen}> 
       <Form ref={formRef}>
        <h1>Novo Prato</h1>
-       <Input />
-       <Input />
-       <Input />
-       <Input />
 
-       <button>salvar</button>
+       <label>link da imagem do prato</label>
+       <Input name="image" placeholder="cole aqui"/>
+       <br />
+       <label> Nome do prato</label>
+       <Input name="name" placeholder="Ex: moda Italiana"/>
+       <br />
+       <label> Valor do prato </label>
+       <Input name="price" placeholder="Ex: 19.90"/>
+       <br />
+       <label>Descriçao do prato</label>
+       <Input name="description" placeholder="Descriçao"/>
+
+       <button type="submit" data-testid="add-food-button">
+        <p className="text">Adicionar Prato</p>
+       </button>
       </Form>
     </Modal>
  )
